6caea73124fd1b0ee98c197b1346ca86598dd286
samusshu <sgaponte@uniboyaca.edu.co>
2026-06-13 09:56:49 -0500

    samuel: [Arreglo bugs]

--- Js/Ui/Tables.js
+++ Js/Ui/Tables.js
@@ -27,24 +27,10 @@ const Tables = (() => {
           <div class="action-btns">
             <button class="btn btn--icon btn--edit" title="Editar"
               onclick="Forms.abrirEditarEmpleado(${emp.id})">
-              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"
-                fill="none" stroke="currentColor" stroke-width="2.5"
-                stroke-linecap="round" stroke-linejoin="round">
-                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
-                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-              </svg>
               Editar
             </button>
             <button class="btn btn--icon btn--delete" title="Eliminar"
               onclick="Forms.confirmarEliminarEmpleado(${emp.id}, '${(emp.nombres||'')+' '+(emp.apellidos||'')}')">
-              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"
-                fill="none" stroke="currentColor" stroke-width="2.5"
-                stroke-linecap="round" stroke-linejoin="round">
-                <polyline points="3 6 5 6 21 6"/>
-                <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/>
-                <path d="M10 11v6"/><path d="M14 11v6"/>
-                <path d="M9 6V4h6v2"/>
-              </svg>
               Eliminar
             </button>
           </div>
@@ -111,24 +97,10 @@ const Tables = (() => {
           <div class="action-btns">
             <button class="btn btn--icon btn--edit" title="Editar"
               onclick="Forms.abrirEditarIncapacidad(${inc.id})">
-              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"
-                fill="none" stroke="currentColor" stroke-width="2.5"
-                stroke-linecap="round" stroke-linejoin="round">
-                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
-                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-              </svg>
               Editar
             </button>
             <button class="btn btn--icon btn--delete" title="Eliminar"
               onclick="Forms.confirmarEliminarIncapacidad(${inc.id})">
-              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"
-                fill="none" stroke="currentColor" stroke-width="2.5"
-                stroke-linecap="round" stroke-linejoin="round">
-                <polyline points="3 6 5 6 21 6"/>
-                <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/>
-                <path d="M10 11v6"/><path d="M14 11v6"/>
-                <path d="M9 6V4h6v2"/>
-              </svg>
               Eliminar
             </button>
           </div>
@@ -192,24 +164,10 @@ const Tables = (() => {
           <div class="action-btns">
             <button class="btn btn--icon btn--edit" title="Editar"
               onclick="Forms.abrirEditarSeguimiento(${seg.id})">
-              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"
-                fill="none" stroke="currentColor" stroke-width="2.5"
-                stroke-linecap="round" stroke-linejoin="round">
-                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
-                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-              </svg>
               Editar
             </button>
             <button class="btn btn--icon btn--delete" title="Eliminar"
               onclick="Forms.confirmarEliminarSeguimiento(${seg.id})">
-              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"
-                fill="none" stroke="currentColor" stroke-width="2.5"
-                stroke-linecap="round" stroke-linejoin="round">
-                <polyline points="3 6 5 6 21 6"/>
-                <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/>
-                <path d="M10 11v6"/><path d="M14 11v6"/>
-                <path d="M9 6V4h6v2"/>
-              </svg>
               Eliminar
             </button>
           </div>
